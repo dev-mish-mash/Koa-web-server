878268c67a865dda3749d3fda0a422b02cb00df4
HoseungShin <nwdt3309@gmail.com>
2020-07-24 23:59:28 +0900

    [feat] 메서드별로 api 분리

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,9 @@
 const Router = require('koa-router');
 
 const api = new Router();
+const books = require('./books');
 
-api.get('/books', (ctx, next) => {
-    ctx.body = 'GET ' + ctx.request.path;
-});
+api.use('/books', books.routes());
 
 module.exports = api;
 
