f03e5f2667540aeb666a0bad25636059a4ec4b89
HoseungShin <nwdt3309@gmail.com>
2020-07-27 03:06:54 +0900

    [feat] rest api완성

--- src/api/books/books.controller.js
+++ src/api/books/books.controller.js
@@ -1,6 +1,11 @@
 const Book = require('models/book');
 
 
+const Joi = require('joi');
+
+const { Types: { ObjectId } } = require('mongoose');
+
+
 exports.get = async (ctx) => {
     const { id } = ctx.params;
 
@@ -68,12 +73,12 @@ exports.create = async (ctx) => {
 };
 
 exports.delete = async (ctx) => {
-    const {id} = ctx.params;
+    const { id } = ctx.params;
 
     try {
         await Book.findByIdAndRemove(id).exec();
-    } catch(e) {
-        if(e.name === 'CastError') {
+    } catch (e) {
+        if (e.name === 'CastError') {
             ctx.status = 400;
             return;
         }
@@ -82,10 +87,61 @@ exports.delete = async (ctx) => {
     ctx.status = 204;
 };
 
-exports.replace = (ctx) => {
-    ctx.body = 'replaced';
+exports.replace = async (ctx) => {
+
+    const { id } = ctx.params;
+
+    if (!ObjectId.isValid(id)) {
+        ctx.status = 400;
+        return;
+    }
+
+    const schema = Joi.object().keys({
+        title: Joi.string().required(),
+        authors: Joi.array().items(Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().email().required()
+        })),
+        publishedDate: Joi.date().required(),
+        price: Joi.number().required(),
+        tags: Joi.array().items((Joi.string()).required())
+    });
+
+    const result = Joi.validate(ctx.request.body, schema);
+
+    if (result.error) {
+        ctx.status = 400;
+        ctx.body = result.error;
+        return;
+    }
+
+    let book;
+
+    try {
+        book = await book.findByIdAndUpdate(id, ctx.request.body, {
+            upsert: true,
+            new: true
+        });
+    } catch (e) {
+        return ctx.throw(500, e);
+    }
+    ctx.body = book;
 };
 
-exports.update = (ctx) => {
-    ctx.body = 'updated';
+exports.update = async (ctx) => {
+    if (!ObjectId.isValid(id)) {
+        ctx.status = 400;
+        return;
+    }
+
+    let book;
+
+    try {
+        book = await Book.findByIdAndUpdate(id, ctx.request.body {
+            new: true
+        });
+    } catch (e) {
+        return ctx.throw(500, e);
+    }
+    ctx.body = book;
 };
--- src/api/books/index.js
+++ src/api/books/index.js
@@ -9,9 +9,9 @@ books.post('/', booksCtrl.create);
 
 books.delete('/', booksCtrl.delete);
 
-books.put('/', booksCtrl.replace);
+books.put('/:id', booksCtrl.replace);
 
-books.patch('/', booksCtrl.update);
+books.patch('/:id', booksCtrl.update);
 
 books.get('/:id', booksCtrl.get);
 
