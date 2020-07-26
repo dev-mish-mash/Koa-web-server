03fb6398f47a54dcb4e211eceb4e3ab324b24361
HoseungShin <nwdt3309@gmail.com>
2020-07-26 18:24:54 +0900

    [feat] 몽구스 DB 서버 구축하고 index js 에 추가

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+require('dotenv').config();
 
 const app = new Koa();
 const router = new Router();
 const api = require('./api');
 
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(process.env.MONGO_URI, {
+    useMongoClient: true
+}).then(
+    (response) => {
+        console.log('Successfully connected to mongodb');
+    }
+).catch(e => {
+    console.error(e);
+})
+
+const port = process.env.PORT || 4000;
+
 router.use('/api', api.routes());
 
 app.use(router.routes()).use(router.allowedMethods());
 
-
 // 추후 추가구현
-app.listen(4000, () => {
-    console.log('koa server is listening to port 4000');
+app.listen(port, () => {
+    console.log('koa server is listening to port ' + port);
 })
