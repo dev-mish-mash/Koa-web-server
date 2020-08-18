6b3392aaf4c2cb80641e2a903100d3794d9bfaea
HoseungShin <nwdt3309@gmail.com>
2020-08-18 23:59:15 +0900

    [refactor] 중복문 삭제

--- src/api/books/books.controller.js
+++ src/api/books/books.controller.js
@@ -89,26 +89,7 @@ exports.delete = async (ctx) => {
 
 exports.replace = async (ctx) => {
 
-    const { id } = ctx.params;
-
-    if (!ObjectId.isValid(id)) {
-        ctx.status = 400;
-        return;
-    }
-
-    const schema = Joi.object().keys({
-        title: Joi.string().required(),
-        authors: Joi.array().items(Joi.object().keys({
-            name: Joi.string().required(),
-            email: Joi.string().email().required()
-        })),
-        publishedDate: Joi.date().required(),
-        price: Joi.number().required(),
-        tags: Joi.array().items((Joi.string()).required())
-    });
-
     const result = Joi.validate(ctx.request.body, schema);
-    // false commit
     if (result.error) {
         ctx.status = 400;
         ctx.body = result.error;
